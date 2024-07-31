e898cf74834242fbc10e97421677de94b1fce18c
DarioOliviero24 <dario_oliviero@yahoo.com>
2024-07-31 18:54:43 +0200

    milestone0

---
+++ js/script.js
@@ -0,0 +1,45 @@
+/*DATI PER ESERCIZIO:
+
+Name            Role                    Image
+Wayne Barnett	Founder & CEO	        wayne-barnett-founder-ceo.jpg
+Angela Caroll	Chief Editor	        angela-caroll-chief-editor.jpg
+Walter Gordon	Office Manager	        walter-gordon-office-manager.jpg
+Angela Lopez	Social Media Manager	angela-lopez-social-media-manager.jpg
+Scott Estrada	Developer	            scott-estrada-developer.jpg
+Barbara Ramos	Graphic Designer	    barbara-ramos-graphic-designer.jpg
+*/
+
+const ourTeam = [
+    {
+    Name : 'Wayne Barnett',
+    role : 'Founder & CEO',
+    image: 'wayne-barnett-founder-ceo.jpg'  
+    },
+    {
+    Name : 'Angela Caroll',
+    role : 'Chief Editor',
+    image: 'angela-caroll-chief-editor.jpg' 
+    },
+    {
+    Name : 'Walter Gordon',
+    role : 'Office Manager',
+    image: 'walter-gordon-office-manager.jpg'
+    },
+    {
+    Name : 'Angela Lopez',
+    role : 'Social Media Manager',
+    image: 'angela-lopez-social-media-manager.jpg' 
+    },
+    {
+    Name : 'Scott Estrada',
+    role : 'Developer',
+    image: 'scott-estrada-developer.jpg'
+    },
+    {
+    Name : 'Barbara Ramos',
+    role : 'Graphic Designer',
+    image: 'barbara-ramos-graphic-designer.jpg' 
+    },
+   
+];
+
